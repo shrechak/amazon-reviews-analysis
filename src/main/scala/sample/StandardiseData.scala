b7ded6b574732d8f54a32013c12c5b964f33f598
Shreya <c.shreya2204@gmail.com>
2018-04-26 12:16:00 +0530

    Update standardistaion for reviews also

--- src/main/scala/sample/StandardiseData.scala
+++ src/main/scala/sample/StandardiseData.scala
@@ -10,12 +10,13 @@ object StandardiseData extends App{
 
   val cmdArgs = Args(args)
 
+  val metadataInputPath = cmdArgs("metadata")
   val reviewsInputPath = cmdArgs("reviews")
   val outputPath = cmdArgs("output")
 
   val spark = SparkSession.builder().getOrCreate()
 
-  val price = spark.read.parquet(reviewsInputPath)
+  val price = spark.read.parquet(metadataInputPath)
 
   def getCategory(input: String) = {
     val pattern = """([a-z_]+)""".r
@@ -29,6 +30,8 @@ object StandardiseData extends App{
 
   val udf1 = udf{source: String => getCategory(source)}
   val udf2 = udf{source: String => getSalesRank(source)}
+
+  //writing standardised metadata
 
   val schema = price.schema
   val allRecords = price.filter("_corrupt_record is null")
@@ -47,6 +50,19 @@ object StandardiseData extends App{
     .drop("sample", "salesRankJson")
     .withColumn("givenTopCategory", getGivenTopCategory(expr("categories[0][0]")))
     .withColumn("finalTopCategory", expr("case when givenTopCategory like 'unknown' and salesRankTopCategory is not null then salesRankTopCategory else givenTopCategory end"))
+    .drop("givenTopCategory")
     .write.option("codec", "snappy")
-    .parquet(outputPath)
+    .parquet(outputPath+"/reviews")
+
+
+  //writing standardised reviews data
+
+  val getHelpfulPercent = udf{x: Seq[Long] => if (x.head != 0) x.head.toFloat/x.last else 0}
+
+  spark.read.parquet(reviewsInputPath)
+    .withColumn("helpfulFrac", getHelpfulPercent(col("helpful")))
+    .write.option("codec", "snappy")
+    .parquet(outputPath+"/reviews")
+
+
 }
